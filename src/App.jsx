56e967eae456690cf2496b524f812491c00b30f0
Shubham <shubhampatil1356@gmail.com>
2025-09-22 21:12:49 +0530

    Inital setup for react(vite) and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center text-6xl font-semibold text-blue-500'>
+      Dashboard
+    </div>
+  );
+};
+
+export default App;
